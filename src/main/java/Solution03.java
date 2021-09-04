432b9e90258aa56f3e34377bd979f0580ff93865
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-09-04 19:39:07 -0400

    Wrote the whole solution, first in psuedocode, then in actual code. Uses a scanner to fetch 2 strings, which are then stored and concatenated. I also remembered to close the scanner in this one.

--- src/main/java/Solution03.java
+++ src/main/java/Solution03.java
@@ -1,7 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 David Beers
+ */
 
+/*
+*   print a user prompt for a quote
+*   store the quote
+    print a user prompt for an attribution
+    store the attribution
+    print an output that concatenates the attribution and quote
+    the quote in the output will be wrapped in \" to escape the quotation marks*/
+
+import java.util.Scanner;
 
 public class Solution03 {
     public static void main(String []args){
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input a quote");
+        String quote = input.nextLine();
+        System.out.println("Who said that quote?");
+        String attribution = input.nextLine();
+        System.out.println(attribution + " says: \"" + quote + "\"");
+        input.close();
     }
 }
